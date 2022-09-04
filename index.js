ca13226f099ea2341ece32172973d947369ff89a
lorisobi <lorisobi@outlook.com>
2022-09-04 07:38:02 +0200

    sw again

--- index.js
+++ index.js
@@ -4,7 +4,24 @@
          console.log("Version 20210506.18.22")
          console.log(localStorage.tasks);
 
-         
+         // This is the "Offline copy of assets" service worker
+
+const CACHE = "pwabuilder-offline";
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+workbox.routing.registerRoute(
+  new RegExp('/*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE
+  })
+);
 
          if (localStorage.length == 0){
             word = "";
