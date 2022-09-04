a28a43fe6d1a7e9cf513224271097dcfcdd34c82
lorisobi <lorisobi@outlook.com>
2022-09-04 07:26:39 +0200

    updated

--- index.js
+++ index.js
@@ -4,9 +4,29 @@
          console.log("Version 20210506.18.22")
          console.log(localStorage.tasks);
 
-         if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('service-worker.js');
-          }
+         const registerServiceWorker = async () => {
+            if ("serviceWorker" in navigator) {
+              try {
+                const registration = await navigator.serviceWorker.register("/sw.js", {
+                  scope: "/",
+                });
+                if (registration.installing) {
+                  console.log("Service worker installing");
+                } else if (registration.waiting) {
+                  console.log("Service worker installed");
+                } else if (registration.active) {
+                  console.log("Service worker active");
+                }
+              } catch (error) {
+                console.error(`Registration failed with ${error}`);
+              }
+            }
+          };
+          
+          // …
+          
+          registerServiceWorker();
+          
 
          if (localStorage.length == 0){
             word = "";
